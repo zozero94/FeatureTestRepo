7f9c3800257d3b79837935085e129beda915d124
zojae031 <android_develop@kakao.com>
2020-08-02 19:23:37 +0900

    프로젝트 디펜던시 사전작업 끝.

--- data/src/main/java/com/example/data/repository/RepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/RepositoryImpl.kt
@@ -1,11 +1,10 @@
 package com.example.data.repository
 
 import com.example.domain.repository.Repository
-import timber.log.Timber
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor() : Repository {
     override fun requestApi() {
-        Timber.e("으이이잉???")
+        //todo request Api
     }
 }
