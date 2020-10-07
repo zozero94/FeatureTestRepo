74262f95c3aa64154aa4a103fa2613bdd39c63c4
zojae031 <android_develop@kakao.com>
2020-10-07 13:09:25 +0900

    [#1] Gesture Listener를 통한 text Index 받아오기

--- MyApplication/app/src/main/java/com/example/myapplication/FlexAdapter.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/FlexAdapter.kt
@@ -1,38 +1,21 @@
 package com.example.myapplication
 
-import android.graphics.Color
-import android.text.Spannable
 import android.text.SpannableString
-import android.text.TextPaint
 import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
-import android.text.style.ForegroundColorSpan
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.annotation.MainThread
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.addTo
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.list_item.view.*
-import java.text.BreakIterator
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 class FlexAdapter(@LayoutRes private val layoutRes: Int) : RecyclerView.Adapter<ViewHolder>() {
     private val listItem = LinkedList<String>()
     var onClick: ((Int) -> Unit)? = null
 
-    override fun onViewDetachedFromWindow(holder: ViewHolder) {
-        super.onViewDetachedFromWindow(holder)
-        holder.clearDisposable()
-    }
 
     @MainThread
     fun replaceItem(list: List<String>) {
@@ -43,12 +26,7 @@ class FlexAdapter(@LayoutRes private val layoutRes: Int) : RecyclerView.Adapter<
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
-        ).apply {
-
-//            itemView.setOnClickListener {
-//                onClick?.invoke(listItem[adapterPosition])
-//            }
-        }
+        )
     }
 
     override fun getItemCount(): Int = listItem.size
@@ -59,90 +37,21 @@ class FlexAdapter(@LayoutRes private val layoutRes: Int) : RecyclerView.Adapter<
         }
 
 
-
 }
 
 
 data class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
     LayoutContainer {
-    private var previous = 0
     var textSelectedListener: ((Int) -> Unit)? = null
     private lateinit var spans: SpannableString
-    private val disposable= CompositeDisposable()
 
 
     fun bind(s: String) {
         containerView.item.text = s
         spans = containerView.item.text as SpannableString
-        setSpannable(s)
-//        startHighLight()
         containerView.item.movementMethod = LinkMovementMethod.getInstance()
     }
 
-    fun clearDisposable(){
-        disposable.clear()
-    }
 
-    private fun startHighLight() {
-        Observable.interval(500, TimeUnit.MILLISECONDS)
-            .map {
-                it + 1
-            }
-            .map { it.toInt() }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                spans.setSpan(
-                    ForegroundColorSpan(
-                        ContextCompat.getColor(
-                            containerView.context,
-                            R.color.red
-                        )
-                    ), previous, previous + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                )
-                spans.setSpan(
-                    ForegroundColorSpan(
-                        ContextCompat.getColor(
-                            containerView.context,
-                            R.color.colorPrimaryDark
-                        )
-                    ), it, it + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                )
-                previous = it
-            }.addTo(disposable)
-    }
 
-    private fun setSpannable(text: CharSequence) {
-        val iterator = BreakIterator.getWordInstance().apply {
-            setText(text.toString())
-        }
-        var start = iterator.first()
-        var end = iterator.next()
-
-        while (end != BreakIterator.DONE) {
-            spanClickable(start, end)
-            start = end
-            end = iterator.next()
-        }
-    }
-
-    private fun spanClickable(start: Int, end: Int) {
-        spans.setSpan(getClickableSpan(start), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        textSelectedListener = {
-            spans.setSpan(
-                ForegroundColorSpan(Color.RED),
-                start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        }
-    }
-
-    private fun getClickableSpan(start: Int): ClickableSpan {
-        return object : ClickableSpan() {
-            override fun onClick(widget: View) {
-                Log.d("zero", "onclick : $start")
-                textSelectedListener?.invoke(start)
-            }
-
-            override fun updateDrawState(ds: TextPaint) {
-            }
-        }
-    }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,52 +3,24 @@ package com.example.myapplication
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import java.text.BreakIterator
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private val flexAdapter =
-        FlexAdapter(R.layout.list_item).apply {
-            onClick = {
-                Toast.makeText(applicationContext, it, Toast.LENGTH_SHORT).show()
-            }
-        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        with(flexBox) {
-            adapter = flexAdapter
-            layoutManager = LinearLayoutManager(context)
-        }
 
-        button.setOnClickListener {
-            val fuckingLongString = (1..10).map {
-                getString(R.string.long_text)
-            }.toString()
-            val list = separateSentence(fuckingLongString)
-            flexAdapter.replaceItem(list)
+        val fuckingLongString = (1..10).map {
+            getString(R.string.long_text)
+        }.toString()
+        textView.text = fuckingLongString
+        textView.setTextSelectListener {
+            Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
         }
 
     }
 
 
-    private fun separateSentence(text: CharSequence): List<String> {
-        val resultList = LinkedList<String>()
-        val iterator = BreakIterator.getSentenceInstance().apply {
-            setText(text.toString())
-        }
-        var start = iterator.first()
-        var end = iterator.next()
-
-        while (end != BreakIterator.DONE) {
-            val word = text.substring(start, end)
-            resultList.add(word)
-            start = end
-            end = iterator.next()
-        }
-        return resultList
-    }
 }
