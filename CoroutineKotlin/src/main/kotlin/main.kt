9508f2d860d5c71624101bb70d81754c71fd0436
zojae031 <android_develop@kakao.com>
2020-10-20 12:01:22 +0900

    60분 넘으면 무조건 10으로 떨구기

--- CoroutineKotlin/src/main/kotlin/main.kt
+++ CoroutineKotlin/src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main() {
-
+    var estimatedTime=0
+    val time =3600001L
+    if (time > 3600000L) println(estimatedTime)
+    for (i in 0 until time step 360000) { // 6분간격으로 시간을 나눈다.
+        if (estimatedTime >= 10) break
+        estimatedTime++
+    }
+    print(estimatedTime)
 }
