3667db18757ba3b5f019e57fc4ce0a8b286f62c1
zojae031 <android_develop@kakao.com>
2020-12-08 19:14:58 +0900

    [#9] 액티비티, 뷰모델 추가

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/mediapipe/MediaPipeActivity.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.ui.mediapipe
+
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMediapipeBinding
+import com.example.myapplication.ui.base.BaseActivity
+
+class MediaPipeActivity: BaseActivity<ActivityMediapipeBinding>() {
+    override val layoutId: Int = R.layout.activity_mediapipe
+
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/mediapipe/MediaPipeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.mediapipe
+
+import androidx.lifecycle.ViewModel
+
+class MediaPipeViewModel:ViewModel() {
+
+}
