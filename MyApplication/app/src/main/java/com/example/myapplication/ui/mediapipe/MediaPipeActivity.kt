203afe0ad1a6e0022583e87519e941005e1345aa
zojae031 <android_develop@kakao.com>
2020-12-10 20:50:21 +0900

    [#9] Bazel Build Test

--- MyApplication/app/src/main/java/com/example/myapplication/ui/mediapipe/MediaPipeActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/mediapipe/MediaPipeActivity.kt
@@ -7,8 +7,11 @@ import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
 import android.os.Bundle
 import android.util.Log
+import android.util.Size
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
+import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -26,6 +29,7 @@ class MediaPipeActivity : BaseActivity<ActivityMediapipeBinding>() {
     private var previewFrameTexture: SurfaceTexture? = null
     private var previewDisplayView: SurfaceView? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (checkPermission()) {
@@ -35,6 +39,13 @@ class MediaPipeActivity : BaseActivity<ActivityMediapipeBinding>() {
         }
         previewDisplayView = SurfaceView(this)
         setupPreviewDisplayView();
+
+        try {
+            val applicationInfo =
+                packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.e(TAG, "Cannot find application info: $e")
+        }
     }
 
     private fun checkPermission() = PERMISSIONS.all {
@@ -46,12 +57,12 @@ class MediaPipeActivity : BaseActivity<ActivityMediapipeBinding>() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        Log.d("zero", "requestCode : $requestCode , permission : $permissions")
+        Log.d(TAG, "requestCode : $requestCode , permission : $permissions")
     }
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
+        previewDisplayView?.visibility = View.VISIBLE
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
 
@@ -74,10 +85,30 @@ class MediaPipeActivity : BaseActivity<ActivityMediapipeBinding>() {
     }
 
     private fun setupPreviewDisplayView() {
-        previewDisplayView!!.visibility = View.GONE
+        previewDisplayView?.visibility = View.GONE
+        val viewGroup: ViewGroup = binding.previewDisplayLayout
+        viewGroup.addView(previewDisplayView)
+        previewDisplayView?.holder?.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder?) {}
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder?,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                val viewSize = Size(width, height)
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder?) {}
+
+        })
+
     }
 
     companion object {
+        const val TAG = "ZERO"
+
         fun newIntent(context: Context) = Intent(context, MediaPipeActivity::class.java)
         private val PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private const val REQUEST_CODE_PERMISSIONS = 100
