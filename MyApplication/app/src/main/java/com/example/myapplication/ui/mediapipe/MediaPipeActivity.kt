94ed333a52fbc76c1fe5e4cf68436d62d9337e92
zojae031 <android_develop@kakao.com>
2020-12-13 16:39:36 +0900

    [#9] AAR 카메라 실행 (예제)

--- MyApplication/app/src/main/java/com/example/myapplication/ui/mediapipe/MediaPipeActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/mediapipe/MediaPipeActivity.kt
@@ -1,25 +1,23 @@
 package com.example.myapplication.ui.mediapipe
 
-import android.Manifest
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
 import android.os.Bundle
-import android.util.Log
 import android.util.Size
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
-import android.view.ViewGroup
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMediapipeBinding
 import com.example.myapplication.ui.base.BaseActivity
+import com.google.mediapipe.components.CameraHelper.CameraFacing
+import com.google.mediapipe.components.CameraXPreviewHelper
+import com.google.mediapipe.components.ExternalTextureConverter
+import com.google.mediapipe.components.FrameProcessor
+import com.google.mediapipe.components.PermissionHelper
+import com.google.mediapipe.framework.AndroidAssetUtil
+import com.google.mediapipe.glutil.EglManager
 
 
 class MediaPipeActivity : BaseActivity<ActivityMediapipeBinding>() {
@@ -27,29 +25,49 @@ class MediaPipeActivity : BaseActivity<ActivityMediapipeBinding>() {
 
 
     private var previewFrameTexture: SurfaceTexture? = null
-    private var previewDisplayView: SurfaceView? = null
+    private val previewDisplayView: SurfaceView by lazy { SurfaceView(this) }
 
+    private val eglManager: EglManager by lazy { EglManager(null) }
+    private val processor: FrameProcessor by lazy {
+        FrameProcessor(
+            this,
+            eglManager.nativeContext,
+            BINARY_GRAPH_NAME,
+            INPUT_VIDEO_STREAM_NAME,
+            OUTPUT_VIDEO_STREAM_NAME
+        )
+    }
+    private val converter: ExternalTextureConverter by lazy { ExternalTextureConverter(eglManager.context) }
+
+    private var cameraHelper: CameraXPreviewHelper? = null
+
+    init {
+        System.loadLibrary("mediapipe_jni");
+        System.loadLibrary("opencv_java3");
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (checkPermission()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
-        previewDisplayView = SurfaceView(this)
         setupPreviewDisplayView();
 
-        try {
-            val applicationInfo =
-                packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
-        } catch (e: PackageManager.NameNotFoundException) {
-            Log.e(TAG, "Cannot find application info: $e")
+        AndroidAssetUtil.initializeNativeAssetManager(this)
+        processor.videoSurfaceOutput.setFlipY(FLIP_FRAMES_VERTICALLY)
+        PermissionHelper.checkAndRequestCameraPermissions(this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        converter.setFlipY(FLIP_FRAMES_VERTICALLY)
+        converter.setConsumer(processor)
+        if (PermissionHelper.cameraPermissionsGranted(this)) {
+            startCamera()
         }
     }
 
-    private fun checkPermission() = PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    override fun onPause() {
+        super.onPause()
+        converter.close()
     }
 
     override fun onRequestPermissionsResult(
@@ -57,60 +75,73 @@ class MediaPipeActivity : BaseActivity<ActivityMediapipeBinding>() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        Log.d(TAG, "requestCode : $requestCode , permission : $permissions")
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-        previewDisplayView?.visibility = View.VISIBLE
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-                }
-
-            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
-
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
-            } catch (e: Exception) {
-                Log.e("startCamera", "use case binding failed")
-            }
-
-        }, ContextCompat.getMainExecutor(this))
-    }
 
     private fun setupPreviewDisplayView() {
-        previewDisplayView?.visibility = View.GONE
-        val viewGroup: ViewGroup = binding.previewDisplayLayout
-        viewGroup.addView(previewDisplayView)
-        previewDisplayView?.holder?.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceCreated(holder: SurfaceHolder?) {}
+        previewDisplayView.visibility = View.GONE
+        binding.previewDisplayLayout.addView(previewDisplayView)
+        previewDisplayView
+            .holder
+            .addCallback(
+                object : SurfaceHolder.Callback {
+                    override fun surfaceCreated(holder: SurfaceHolder) {
+                        processor.videoSurfaceOutput.setSurface(holder.surface)
+                    }
 
-            override fun surfaceChanged(
-                holder: SurfaceHolder?,
-                format: Int,
-                width: Int,
-                height: Int
-            ) {
-                val viewSize = Size(width, height)
+                    override fun surfaceChanged(
+                        holder: SurfaceHolder,
+                        format: Int,
+                        width: Int,
+                        height: Int
+                    ) {
+                        // (Re-)Compute the ideal size of the camera-preview display (the area that the
+                        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+                        // based on the size of the SurfaceView that contains the display.
+                        val viewSize = Size(width, height)
+                        val displaySize: Size =
+                            cameraHelper?.computeDisplaySizeFromViewSize(viewSize) ?: Size(
+                                width,
+                                height
+                            )
+                        // Connect the converter to the camera-preview frames as its input (via
+                        // previewFrameTexture), and configure the output width and height as the computed
+                        // display size.
+                        converter.setSurfaceTextureAndAttachToGLContext(
+                            previewFrameTexture, displaySize.width, displaySize.height
+                        )
+                    }
+
+                    override fun surfaceDestroyed(holder: SurfaceHolder) {
+                        processor.videoSurfaceOutput.setSurface(null)
+                    }
+                })
+    }
+
+
+    private fun startCamera() {
+        cameraHelper = CameraXPreviewHelper().apply {
+            setOnCameraStartedListener { surfaceTexture ->
+                previewFrameTexture = surfaceTexture
+                // Make the display view visible to start showing the preview. This triggers the
+                // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+                previewDisplayView.visibility = View.VISIBLE
             }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder?) {}
-
-        })
-
+            startCamera(this@MediaPipeActivity, CAMERA_FACING,  /*surfaceTexture=*/null)
+        }
     }
 
     companion object {
-        const val TAG = "ZERO"
 
         fun newIntent(context: Context) = Intent(context, MediaPipeActivity::class.java)
-        private val PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-        private const val REQUEST_CODE_PERMISSIONS = 100
+
+        private const val BINARY_GRAPH_NAME = "face_detection_mobile_gpu.binarypb"
+        private const val INPUT_VIDEO_STREAM_NAME = "input_video"
+        private const val OUTPUT_VIDEO_STREAM_NAME = "output_video"
+        private val CAMERA_FACING = CameraFacing.FRONT
+        private const val FLIP_FRAMES_VERTICALLY = true
+
     }
 }
