548cf1d25dd83fe4cffab80aceff10f755f5ffc2
zozero94 <android_develop@kakao.com>
2021-03-25 16:29:32 +0900

    [#13] 문제가 되는 코드 작성
    rxjava3->rxjava2

--- CoroutineKotlin/src/main/kotlin/main.kt
+++ CoroutineKotlin/src/main/kotlin/main.kt
@@ -1,47 +1,85 @@
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.disposables.Disposable
-import io.reactivex.rxjava3.schedulers.Schedulers
-import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 private val disposableMap = mutableMapOf<String, Disposable>()
+private val cancelMap = mutableSetOf<String>()
 
-//todo 1 : 테스크 중간에 dispose 가 되었을 때, 나머지 테스크가 진행이 되는가?
-//todo 2 : 모든 subject emit 이후 disposable 이 dispose 가 되는가??
+private val newThread = Schedulers.from(Executors.newCachedThreadPool())
+
+//todo 1 : 여러 테스크 중간에 하나의 스트림이 dispose 가 되었을 때, 나머지 테스크가 진행이 되는가?
+//todo 2 : 모든 subject emit 이후 disposable 이 dispose 가 되는가?? -> 당연히 안됨.
 fun main() {
 
     val subject = BehaviorSubject.create<String>()
-
     val disposable = subject
+        .observeOn(newThread)
         .concatMapEager {
-            newTask(it).toObservable()
+            prepare(UploadFile(it)).toObservable()
         }
-        .subscribeOn(Schedulers.computation())
+        .concatMapSingle {
+            startUpload(it)
+        }
         .subscribe {
             println("subscribe : $it")
         }
 
-    subject.onNext("task1")
-    subject.onNext("task2")
-    subject.onNext("task3")
-    subject.onNext("task4")
-    Thread.sleep(1000)
-    disposableMap.forEach {
-        println("${it.key} ${it.value}")
-    }
-    disposableMap["task2"]?.dispose()
-    println(disposable.isDisposed)
 
-    Thread.sleep(5000)
+    //data 발행한다.
+    Thread {
+        (0..3).forEach {
+            subject.onNext("task$it")
+        }
+    }.start()
 
+
+    //발행 도중에 특정 테스크를 취소한다.
+    Thread {
+        Thread.sleep(6000)
+        println()
+        println("/////////")
+        println("task2 dispose ${disposableMap["task2"]}")
+        disposableMap["task2"]?.dispose()
+        disposableMap.forEach(::println)
+        println("/////////")
+        println()
+
+    }.start()
 
 }
 
-fun newTask(taskId: String): Single<String> {
-    return Single.create<String> {
-        Thread.sleep(2000)
-        it.onSuccess("emit Task $taskId")
+data class UploadFile(val name: String)
+
+val list = listOf(100, 500, 300, 400, 100)
+var index = 0
+fun prepare(file: UploadFile): Single<UploadFile> {
+    return Single.create {
+        println("prepare Start $file")
+        Thread.sleep(list[index++].toLong())
+        println("prepare End $file")
+        println()
+        it.onSuccess(file)
     }
+}
+
+fun startUpload(file: UploadFile): Single<Boolean> {
+    return Observable.timer(2, TimeUnit.SECONDS)
+        .flatMapSingle {
+            Single.create<Boolean> {
+                println("startUpload $file")
+                if (!it.isDisposed) {
+                    it.onSuccess(true)
+                }
+            }
+        }.single(false)
+        .doOnDispose {
+            println("dispose ${file.name}")
+        }
         .doOnSubscribe {
-            disposableMap[taskId] = it
+            disposableMap[file.name] = it
         }
 }
