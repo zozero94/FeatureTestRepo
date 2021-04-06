e20ab0ea3587c282581d4d38d5a19819ac12a7bb
zozero94 <android_develop@kakao.com>
2021-04-06 15:47:45 +0900

    [#14] 프록시 패턴 추가

--- CoroutineKotlin/src/main/kotlin/main.kt
+++ CoroutineKotlin/src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 fun main() {
+
     val concreteClass = ConcreteClass()
     val delegation = Delegation(concreteClass)
     delegation.doSomeThing()
@@ -6,6 +7,10 @@ fun main() {
     println("---------------------------------")
     val delegationKotlin = DelegationKotlin(concreteClass)
     delegationKotlin.doSomeThing()
+
+    println("---------------------------------")
+    val proxy = Proxy()
+    proxy.doSomeThing()
 }
 
 interface Component {
@@ -19,7 +24,11 @@ class ConcreteClass : Component {
     }
 }
 
-
+/**
+ * 데코레이트 패턴
+ * 런타임에 구체 클래스가 지정 됨
+ * @see Component
+ */
 class Delegation(private val origin: Component) : Component {
 
     override fun doSomeThing() {
@@ -30,4 +39,19 @@ class Delegation(private val origin: Component) : Component {
 
 class DelegationKotlin(private val origin: Component) : Component by origin {
     // Component 의 기능을 오버라이드 하지 않아도, 바이패스는 자동지원한다.
+}
+
+/**
+ * 프록시 패턴 : 레이지 하게 생성 할 수 있음
+ * 컴파일 시간에 구체 클래스가 지정 됨
+ * @see ConcreteClass
+ */
+class Proxy : Component {
+    private lateinit var proxy: Component
+    override fun doSomeThing() {
+        proxy = ConcreteClass()
+        println("이거슨 프록시에서 호출되는 것이란다.")
+        proxy.doSomeThing()
+    }
+
 }
