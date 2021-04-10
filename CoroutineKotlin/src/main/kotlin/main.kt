8059b3bb23fdce428af3d1b48fff9a4fc7d7c550
zozero94 <android_develop@kakao.com>
2021-04-10 14:11:40 +0900

    ReadWriteProperty 사용해보기

--- CoroutineKotlin/src/main/kotlin/main.kt
+++ CoroutineKotlin/src/main/kotlin/main.kt
@@ -1,7 +1,32 @@
-import design.ProxyAndDecorate
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 fun main() {
+    val example by Example<Int>()
+    val b by Delegate()
+    println(b)
+    println(example)
 
-    ProxyAndDecorate.test()
 }
+class Example<T : Any> : ReadWriteProperty<Any?, T?> {
+    var item: T? = null
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T?) {
+        println(property.name)
+        item = value
+    }
 
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T? {
+        println(property.name)
+        return item
+    }
+
+}
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("$value has been assigned to '${property.name}' in $thisRef.")
+    }
+}
