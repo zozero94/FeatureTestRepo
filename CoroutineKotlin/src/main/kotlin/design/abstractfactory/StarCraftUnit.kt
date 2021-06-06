603fa1f634d4e91af9c9f37716d0614b829bb25d
zozero94 <android_develop@kakao.com>
2021-06-06 19:10:35 +0900

    [#16] 출력 결과에 상태값 표시(미네랄/가스)

--- CoroutineKotlin/src/main/kotlin/design/abstractfactory/StarCraftUnit.kt
+++ CoroutineKotlin/src/main/kotlin/design/abstractfactory/StarCraftUnit.kt
@@ -11,28 +11,29 @@ abstract class TerranUnit : StarCraftUnit() {
     sealed class BarrackUnit : TerranUnit() {
         class Marine(override val mineral: Int = 50, override val gas: Int = 0) : BarrackUnit() {
             override fun attack() {
-                println("$this : 투둥 투둥")
+                println("$this : 투둥 투둥 $mineral/$gas")
             }
         }
 
         class Firebat(override val mineral: Int = 50, override val gas: Int = 25) : BarrackUnit() {
             override fun attack() {
-                println("$this 빨간바지")
+                println("$this 빨간바지 $mineral/$gas")
             }
         }
 
         class Medic(override val mineral: Int = 50, override val gas: Int = 25) : BarrackUnit() {
             override fun attack() {
-                println("$this 아앙...❤")
+                println("$this 아앙...❤ $mineral/$gas")
             }
         }
 
         class Ghost(override val mineral: Int = 25, override val gas: Int = 75) : BarrackUnit() {
             override fun attack() {
-                println("$this 수레기")
+                println("$this 수레기 $mineral/$gas")
             }
         }
     }
+
 }
 
 abstract class ProtossUnit : StarCraftUnit() {
@@ -40,25 +41,25 @@ abstract class ProtossUnit : StarCraftUnit() {
     sealed class GateWayUnit : ProtossUnit() {
         class Zealot(override val mineral: Int = 100, override val gas: Int = 0) : GateWayUnit() {
             override fun attack() {
-                println("$this 땈땈")
+                println("$this 땈땈 $mineral/$gas")
             }
         }
 
         class Dragon(override val mineral: Int = 125, override val gas: Int = 50) : GateWayUnit() {
             override fun attack() {
-                println("$this 니조랄")
+                println("$this 니조랄 $mineral/$gas")
             }
         }
 
         class DarkTemplar(override val mineral: Int = 125, override val gas: Int = 100) : GateWayUnit() {
             override fun attack() {
-                println("$this 스컹스컹")
+                println("$this 스컹스컹 $mineral/$gas")
             }
         }
 
         class HighTemplar(override val mineral: Int = 50, override val gas: Int = 150) : GateWayUnit() {
             override fun attack() {
-                println("$this 지직지직")
+                println("$this 지직지직 $mineral/$gas")
             }
         }
     }
